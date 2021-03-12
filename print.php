c3d24bb9c3c978de6c093a3c4a8ce07f7deb02ab
achierichie <achie.richie78@gmail.com>
2021-03-12 21:04:38 +0530

    update

---
+++ print.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__.'/public/inc/Loader.php';
+require_once __DIR__.'/libs/bootstrap.php';
+// Render our view
+$message = 'hello world!';
+$sound = 'p';
+echo $view->render('home/print.twig', [
+    'message' => $message,
+    'sound' => $sound
+    ]);
+
