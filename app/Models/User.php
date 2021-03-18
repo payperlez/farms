76ad1b98dfee09e331d04ba07986c88afe125d30
payperlez <info@payperlez.com>
2021-03-18 14:11:42 +0000

    working on Farmer model

---
+++ app/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+class Farmer extends DUtils
+{
+    public function registerFarmer()
+    {
+        try {
+            //code...
+        } catch (Exception $e) {
+            //throw $th;
+        }
+    }
+}
