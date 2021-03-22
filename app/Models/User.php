af7e5d470e5c16a5574528524d634b72d9dbeb3d
payperlez <info@payperlez.com>
2021-03-22 14:37:15 +0000

    farmer id generator don

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,45 +24,59 @@ class Farmer extends DUtils
         $buycompany
     ) {
         try {
-            $this->register = $this->query(
-                'INSERT INTO farms.farmers(fullname, age, gender, community, region, household_status, no_dependents, marital_status, employment_status, educational_level, yrs_farming, source_income, lang, no_farms, trainee_status, level_training, phone, momo, buying_company ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?)',
-                $fname,
-                $age,
-                $gender,
-                $comm,
-                $region,
-                $hhold,
-                $dependents,
-                $marital,
-                $empStatus,
-                $eduLevel,
-                $yrsfarm,
-                $srcincome,
-                $lang,
-                $nofarms,
-                $traineeStatus,
-                $trainLevel,
-                $phone,
-                $momo,
-                $buycompany
-            );
-            if ($this->register) {
-                $this->alert =
-                    '<script> alert(" Inserted successfully ")</script>';
-            }
+            $this->farmerId($fname, $comm, $region);
+            echo $this->farmerId;
+            die();
+            // $this->register = $this->query(
+            //     'INSERT INTO farms.farmers(fullname, age, gender, community, region, household_status, no_dependents, marital_status, employment_status, educational_level, yrs_farming, source_income, lang, no_farms, trainee_status, level_training, phone, momo, buying_company ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?)',
+            //     $fname,
+            //     $age,
+            //     $gender,
+            //     $comm,
+            //     $region,
+            //     $hhold,
+            //     $dependents,
+            //     $marital,
+            //     $empStatus,
+            //     $eduLevel,
+            //     $yrsfarm,
+            //     $srcincome,
+            //     $lang,
+            //     $nofarms,
+            //     $traineeStatus,
+            //     $trainLevel,
+            //     $phone,
+            //     $momo,
+            //     $buycompany
+            // );
+            // if ($this->register) {
+            //     $this->alert =
+            //         '<script> alert(" Inserted successfully ")</script>';
+            // }
         } catch (Exception $e) {
             //throw $th;
         }
     }
 
-    // farm id generator
-    public function farmerId()
+    // farmer id generator
+    public function farmerId($fname, $comm, $region)
     {
         try {
-            $prefix = 'INTERN/';
-            $year = date('y/');
+            $name = substr($fname, 0, 3);
+            $comm = substr($comm, 0, 3);
             $num = substr(str_shuffle('01234567899876543210'), 0, 4);
-            return $this->card_num = strtoupper($prefix . $year . $num);
+            return $this->farmerId = strtoupper(
+                $name . '/' . $comm . '/' . $region . '/' . $num
+            );
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    // farms id generato
+    public function farmId($farmerId)
+    {
+        try {
         } catch (\Throwable $th) {
             return false;
         }
