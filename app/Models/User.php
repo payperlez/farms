d8bc318032df6357cac281a30c9187cf00f6af4b
payperlez <info@payperlez.com>
2021-03-22 17:06:09 +0000

    farms id generator , data inserted

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,34 +25,39 @@ class Farmer extends DUtils
     ) {
         try {
             $this->farmerId($fname, $comm, $region);
-            echo $this->farmerId;
-            die();
-            // $this->register = $this->query(
-            //     'INSERT INTO farms.farmers(fullname, age, gender, community, region, household_status, no_dependents, marital_status, employment_status, educational_level, yrs_farming, source_income, lang, no_farms, trainee_status, level_training, phone, momo, buying_company ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?)',
-            //     $fname,
-            //     $age,
-            //     $gender,
-            //     $comm,
-            //     $region,
-            //     $hhold,
-            //     $dependents,
-            //     $marital,
-            //     $empStatus,
-            //     $eduLevel,
-            //     $yrsfarm,
-            //     $srcincome,
-            //     $lang,
-            //     $nofarms,
-            //     $traineeStatus,
-            //     $trainLevel,
-            //     $phone,
-            //     $momo,
-            //     $buycompany
-            // );
-            // if ($this->register) {
-            //     $this->alert =
-            //         '<script> alert(" Inserted successfully ")</script>';
-            // }
+            $farmerId = $this->farmerId;
+            $this->farmId($farmerId, $nofarms);
+            $farmId = json_encode($this->farmId);
+            // echo $farmId;
+            // die();
+            $this->register = $this->query(
+                'INSERT INTO farms.farmers(fullname, age, gender, community, region, household_status, no_dependents, marital_status, employment_status, educational_level, yrs_farming, source_income, lang, no_farms, trainee_status, level_training, phone, momo, farmer_id, farm_id, buying_company ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?)',
+                $fname,
+                $age,
+                $gender,
+                $comm,
+                $region,
+                $hhold,
+                $dependents,
+                $marital,
+                $empStatus,
+                $eduLevel,
+                $yrsfarm,
+                $srcincome,
+                $lang,
+                $nofarms,
+                $traineeStatus,
+                $trainLevel,
+                $phone,
+                $momo,
+                $farmerId,
+                $farmId,
+                $buycompany
+            );
+            if ($this->register) {
+                $this->alert =
+                    '<script> alert(" Inserted successfully ")</script>';
+            }
         } catch (Exception $e) {
             //throw $th;
         }
@@ -74,9 +79,12 @@ class Farmer extends DUtils
     }
 
     // farms id generato
-    public function farmId($farmerId)
+    public function farmId($farmerId, $nofarms)
     {
         try {
+            for ($i = 1; $i <= $nofarms; $i++) {
+                $this->farmId[] = ['farm' . $i => $farmerId . '/' . $i];
+            }
         } catch (\Throwable $th) {
             return false;
         }
